c0f97845168c091c4a976f07733dd0d84ef42237
Slava <slava.boiko@outlook.com>
2020-09-06 20:26:20 +0100

    Refactor validation api tests

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -2,25 +2,28 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("POST /email/validate", () => {
+
+    const validateRequest = () => request(app).post("/email/validate");
+
     it("should return 200 OK", () => {
-        return request(app).post("/email/validate")
+        return validateRequest()
             .expect(200);
     });
 
     it("responds 400 if email address is not present", () => {
-        return request(app).post("/email/validate")
+        return validateRequest()
             .send({})
             .expect(400, { error: "email is not specified" });
     });
 
     it("responds 400 if email address is null", () => {
-        return request(app).post("/email/validate")
+        return validateRequest()
             .send({email: null})
             .expect(400, { error: "email is not specified" });
     });
 
     it("responds 400 if email address is empty", () => {
-        return request(app).post("/email/validate")
+        return validateRequest()
             .send({email: ""})
             .expect(400, { error: "email cannot be empty string" });
     });
