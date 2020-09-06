a244d04c126f59f4e5b9dc9f0a20b3426b7a13db
Slava <slava.boiko@outlook.com>
2020-09-06 20:25:06 +0100

    Add request body validation to the endpoint

--- src/controllers/validation.ts
+++ src/controllers/validation.ts
@@ -4,6 +4,9 @@ import {Request, Response} from "express";
 
 
 export const emailValidation = (req: Request, res: Response) => {
+    if (req.body.email === null || req.body.email === undefined) res.status(400).json({ error: "email is not specified" });
+    if (req.body.email === "") res.status(400).json({ error: "email cannot be empty string" });
+
     res.json({
         ok: true
     });
--- test/validation.test.ts
+++ test/validation.test.ts
@@ -6,4 +6,22 @@ describe("POST /email/validate", () => {
         return request(app).post("/email/validate")
             .expect(200);
     });
+
+    it("responds 400 if email address is not present", () => {
+        return request(app).post("/email/validate")
+            .send({})
+            .expect(400, { error: "email is not specified" });
+    });
+
+    it("responds 400 if email address is null", () => {
+        return request(app).post("/email/validate")
+            .send({email: null})
+            .expect(400, { error: "email is not specified" });
+    });
+
+    it("responds 400 if email address is empty", () => {
+        return request(app).post("/email/validate")
+            .send({email: ""})
+            .expect(400, { error: "email cannot be empty string" });
+    });
 });
